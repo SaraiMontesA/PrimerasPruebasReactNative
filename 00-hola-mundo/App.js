ad1af582f89eb6e7e3853215af7d95f5dd6697f6
Sarai <SaraiMontesA@gmail.com>
2024-11-08 20:24:07 +0100

    Creando mi primera pantalla de registro

--- 00-hola-mundo/App.js
+++ 00-hola-mundo/App.js
@@ -8,7 +8,8 @@ import {
 	Text,
 	View,
 	Image,
-	Button
+	Button,
+	TextInput
 } from 'react-native';
 
 const icon = require('./assets/icon.png');
@@ -28,7 +29,18 @@ export default function App() {
 					resizeMode: 'contain'
 				}}
 			/>
-			<Text style={styles.pepe}>Hola mundo!</Text>			
+			<Text style={styles.titulo}>Hello</Text>
+			<Text style={styles.subtitulo}>Sign in to your account</Text>	
+			<TextInput
+				style={styles.input}
+				placeholder="Mete tu cuenta"
+			>
+			</TextInput>	
+			<TextInput
+				style={styles.password}
+				placeholder="Mete tu cuenta"
+			>
+			</TextInput>			
 			<Button className="pepe"
 				onPress={() => {
 					console.log('Me estás tocando el botón!');
@@ -57,13 +69,45 @@ export default function App() {
 const styles = StyleSheet.create({
 	container: {
 		flex: 1,
-		backgroundColor: '#880',
+		backgroundColor: '#f1f1f1',
 		alignItems: 'center',
 		justifyContent: 'center',
 	},
 
-	pepe: {
-		fontSize: 40,
-		backgroundColor: '#950',
+	titulo: {
+		fontSize: 80,
+		fontWeight: 'bold',
+		color: '#34434d',
+		// backgroundColor: '#959',
+	},
+
+	subtitulo: {
+		fontSize: 20,
+		color: 'grey'
+		// backgroundColor: '#959',
+	},
+
+	input: {
+		borderRadius: 30,
+		// borderWidth: 1,
+		paddingStart: 25,
+		width: '80%',
+		height: 50,
+		padding: 10,
+		marginTop: 20,
+		backgroundColor: '#fff',
+		shadowColor: 'grey',
+	},
+
+	password: {
+		borderRadius: 30,
+		// borderWidth: 1,
+		paddingStart: 25,
+		width: '80%',
+		height: 50,
+		padding: 10,
+		marginTop: 20,
+		backgroundColor: '#fff',
+		shadowColor: 'grey',
 	},
 });
