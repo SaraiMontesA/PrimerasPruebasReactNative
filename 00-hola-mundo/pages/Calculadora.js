ac56ba6d8e12fca389e97e1b5c076062e35d12e5
Sarai <SaraiMontesA@gmail.com>
2024-11-10 02:28:26 +0100

    Calculadora, parte visual a medias

--- 00-hola-mundo/pages/Calculadora.js
+++ 00-hola-mundo/pages/Calculadora.js
@@ -9,7 +9,8 @@ import {
 	View,
 	Image,
 	Button,
-	TextInput
+	TextInput,
+	TouchableOpacity,
 } from 'react-native';
 
 // ########################################################################
@@ -20,35 +21,95 @@ export const Calculadora = () => {
 	return (
 		<View style={styles.container}>
 
-			<Text style={styles.titulo}>Texto</Text>
+
+			{/* <View style={styles.cajaHorizontal}>
+				<Text style={styles.titulo}>Texto</Text>
+			</View> */}
+
+			<View style={styles.cajaTexto}>
+				<Text style={styles.titulo}>9485.45</Text>
+			</View>
 
 			<View style={styles.cajaHorizontal}>
-				<Button style={styles.button}
-					onPress={() => {
-						console.log('C');
-					}}
-					title={"C"}
-				/> 
-				<Button style={styles.button}
-					onPress={() => {
-						console.log('()');
-					}}
-					title={"()"}
-				/> 
-				<Button style={styles.button}
-					onPress={() => {
-						console.log('%');
-					}}
-					title={"%"}
-				/> 
-				<Button style={styles.button}
-					onPress={() => {
-						console.log('/');
-					}}
-					title={"/"}
-				/> 
+				<TouchableOpacity style={styles.button} onPress={() =>
+					console.log('C')}>
+					<Text style={styles.buttonTextNumero}>C</Text>
+				</TouchableOpacity>
+				<TouchableOpacity style={styles.button} onPress={() => console.log('()')}>
+					<Text style={styles.buttonTextNumero}>( )</Text>
+				</TouchableOpacity>
+				<TouchableOpacity style={styles.button} onPress={() => console.log('%')}>
+					<Text style={styles.buttonTextNumero}>%</Text>
+				</TouchableOpacity>
+				<TouchableOpacity style={styles.button} onPress={() => console.log('/')}>
+					<Text style={styles.buttonTextSigno}>/</Text>
+				</TouchableOpacity>
 			</View>
-			
+
+			<View style={styles.cajaHorizontal}>
+				<TouchableOpacity style={styles.button} onPress={() =>
+					{
+						pepe = pepe + '7';
+						console.log('7');
+					}}>
+					<Text style={styles.buttonTextNumero}>7</Text>
+				</TouchableOpacity>
+				<TouchableOpacity style={styles.button} onPress={() => console.log('8')}>
+					<Text style={styles.buttonTextNumero}>8</Text>
+				</TouchableOpacity>
+				<TouchableOpacity style={styles.button} onPress={() => console.log('9')}>
+					<Text style={styles.buttonTextNumero}>9</Text>
+				</TouchableOpacity>
+				<TouchableOpacity style={styles.button} onPress={() => console.log('X')}>
+					<Text style={styles.buttonTextSigno}>x</Text>
+				</TouchableOpacity>
+			</View>
+
+			<View style={styles.cajaHorizontal}>
+				<TouchableOpacity style={styles.button} onPress={() => console.log('4')}>
+					<Text style={styles.buttonTextNumero}>4</Text>
+				</TouchableOpacity>
+				<TouchableOpacity style={styles.button} onPress={() => console.log('5')}>
+					<Text style={styles.buttonTextNumero}>5</Text>
+				</TouchableOpacity>
+				<TouchableOpacity style={styles.button} onPress={() => console.log('6')}>
+					<Text style={styles.buttonTextNumero}>6</Text>
+				</TouchableOpacity>
+				<TouchableOpacity style={styles.button} onPress={() => console.log('-')}>
+					<Text style={styles.buttonTextSigno}>-</Text>
+				</TouchableOpacity>
+			</View>
+
+			<View style={styles.cajaHorizontal}>
+				<TouchableOpacity style={styles.button} onPress={() => console.log('1')}>
+					<Text style={styles.buttonTextNumero}>1</Text>
+				</TouchableOpacity>
+				<TouchableOpacity style={styles.button} onPress={() => console.log('2')}>
+					<Text style={styles.buttonTextNumero}>2</Text>
+				</TouchableOpacity>
+				<TouchableOpacity style={styles.button} onPress={() => console.log('%')}>
+					<Text style={styles.buttonTextNumero}>3</Text>
+				</TouchableOpacity>
+				<TouchableOpacity style={styles.button} onPress={() => console.log('+')}>
+					<Text style={styles.buttonTextSigno}>+</Text>
+				</TouchableOpacity>
+			</View>
+
+			<View style={styles.cajaHorizontal}>
+				<TouchableOpacity style={styles.button} onPress={() => console.log('+/-')}>
+					<Text style={styles.buttonTextNumero}>+/-</Text>
+				</TouchableOpacity>
+				<TouchableOpacity style={styles.button} onPress={() => console.log('0')}>
+					<Text style={styles.buttonTextNumero}>0</Text>
+				</TouchableOpacity>
+				<TouchableOpacity style={styles.button} onPress={() => console.log(',')}>
+					<Text style={styles.buttonTextNumero}>,</Text>
+				</TouchableOpacity>
+				<TouchableOpacity style={styles.button} onPress={() => console.log('=')}>
+					<Text style={styles.buttonTextSigno}>=</Text>
+				</TouchableOpacity>
+			</View>
+						
 		</View>
 	);
 };
@@ -62,26 +123,52 @@ const styles = StyleSheet.create({
 		flex: 1,
 		width: '100%',
 		height: '100%',
-		backgroundColor: '#1ba89f',
+		backgroundColor: '#000000',
 		alignItems: 'center',
 		justifyContent: 'center',
 	},
 
 	cajaHorizontal: {
 		flexDirection: 'row',
+		width: '80%',
+	},
+
+	cajaTexto: {
+		flexDirection: 'row',
+		justifyContent: 'flex-end',
+		width: '80%',
+		marginBottom: 50,
 	},
 
 	titulo: {
 		fontSize: 60,
 		fontWeight: 'bold',
-		color: '#c9fffc',
-		padding: 20,
+		color: '#ffffff',
 		// backgroundColor: '#959',
 	},
 
 	button: {
-		fontSize: 20,
-		padding: 20,
-		backgroundColor: '#d9fffc',
-	}
+		height: 70,
+		width: 70,
+		margin: 10,
+		// centrar el contenido verticalmente
+		justifyContent: 'center',
+		// alinear el contenido horizontalmente
+		alignItems: 'center',
+		backgroundColor: '#2e2e2e',
+		borderRadius: 50,
+		opacity: 0.7,
+	},
+
+	buttonTextNumero: {
+		fontSize: 26,
+		color: '#fff',
+	},
+
+	buttonTextSigno: {
+		fontSize: 28,
+		// letra negrita
+		fontWeight: 'bold',
+		color: '#fff',
+	},
 });
