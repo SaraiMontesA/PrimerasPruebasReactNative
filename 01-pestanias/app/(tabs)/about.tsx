a825beb8a2866f039517788e335b06c531f0d34e
SaraiMontesA <saraimontesa@gmail.com>
2024-11-14 23:09:31 +0100

    Creada lista de cosas a implementar

--- 01-pestanias/app/(tabs)/about.tsx
+++ 01-pestanias/app/(tabs)/about.tsx
@@ -17,7 +17,10 @@ import { IconSymbol } from '@/components/ui/IconSymbol';
 
 export default function TabTwoScreen() {
 	return (
-		<ParallaxScrollView
+
+	<ThemedView style={{ light: '#D0D0D0', dark: '#353636' }}>
+
+		{/* <ParallaxScrollView
 		headerBackgroundColor={{ light: '#D0D0D0', dark: '#353636' }}
 		headerImage={
 			// <IconSymbol
@@ -27,95 +30,61 @@ export default function TabTwoScreen() {
 			// style={styles.headerImage}
 			// />
 			<Image source={require('@/assets/images/MeLoPidoLogo/MeLoPidoGreen.png')} style={ styles.meLoPidoLogo } />
-		}>
+		}> */}
 
-		<ThemedView style={styles.cajaLogo}>
-			{/* <Image source={require('@/assets/images/MeLoPido.png')} style={{ alignSelf: 'center' }} /> */}
+        <ThemedView style={styles.cajaLogo}>
+            <Image source={require('@/assets/images/MeLoPidoLogo/MeLoPidoPurple.png')} style={styles.meLoPidoLogo} />
+        </ThemedView>
+
+		{/* <ThemedView style={styles.cajaLogo}>
 			<Image source={require('@/assets/images/MeLoPidoLogo/MeLoPidoPurple.png')} style={ styles.meLoPidoLogo } />
-		</ThemedView>
+		</ThemedView> */}
 
 		<ThemedView style={styles.titleContainer}>
-			<ThemedText type="title">Explore</ThemedText>
+			<ThemedText type="title">Cosas a implementar</ThemedText>
 		</ThemedView>
+
+		<Collapsible title="Android, iOS, and web support">					
+			<ThemedText> · Cosa 1</ThemedText>
+			<ThemedText> · Cosa 2</ThemedText>
+			<ThemedText> · Cosa 3</ThemedText>
+			<ThemedText> · Cosa 4</ThemedText>
+		</Collapsible>
+
+		<Collapsible title="Front End">
+			<Collapsible title="Pestaña Lista de deseos">		
+				<ThemedText> · Cosa 1</ThemedText>
+				<ThemedText> · Cosa 2</ThemedText>
+				<ThemedText> · Cosa 3</ThemedText>
+				<ThemedText> · Cosa 4</ThemedText>
+			</Collapsible>
+			<Collapsible title="Pestaña Contactos">		
+				<ThemedText> · ListView con listado de amigos, enlace a sus listas</ThemedText>
+				<ThemedText> · Cosa 2</ThemedText>
+				<ThemedText> · Cosa 3</ThemedText>
+				<ThemedText> · Cosa 4</ThemedText>
+			</Collapsible>
+			<Collapsible title="Pestaña Contactos Detalle">		
+				<ThemedText> · ListView con listado de deseos del contacto elegido (enlaces ciegos)</ThemedText>
+			</Collapsible>
+			<Collapsible title="Pestaña Datos de usuario">		
+				<ThemedText> · Cosa 1</ThemedText>
+				<ThemedText> · Cosa 2</ThemedText>
+				<ThemedText> · Cosa 3</ThemedText>
+				<ThemedText> · Cosa 4</ThemedText>
+			</Collapsible>
+		</Collapsible>
+
+		<Collapsible title="Back End">
+			<Collapsible title="Base de datos">		
+				<ThemedText> · Cosa 1</ThemedText>
+				<ThemedText> · Cosa 2</ThemedText>
+				<ThemedText> · Cosa 3</ThemedText>
+				<ThemedText> · Cosa 4</ThemedText>
+			</Collapsible>
+		</Collapsible>
 		
-
-		<ThemedText>This app includes example code to help you get started.</ThemedText>
-
-		<Collapsible title="File-based routing">
-			<ThemedText>
-			This app has two screens:{' '}
-			<ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> and{' '}
-			<ThemedText type="defaultSemiBold">app/(tabs)/explore.tsx</ThemedText>
-			</ThemedText>
-			<ThemedText>
-			The layout file in <ThemedText type="defaultSemiBold">app/(tabs)/_layout.tsx</ThemedText>{' '}
-			sets up the tab navigator.
-			</ThemedText>
-			<ExternalLink href="https://docs.expo.dev/router/introduction">
-			<ThemedText type="link">Learn more</ThemedText>
-			</ExternalLink>
-		</Collapsible>
-
-		<Collapsible title="Android, iOS, and web support">
-			<ThemedText>
-			You can open this project on Android, iOS, and the web. To open the web version, press{' '}
-			<ThemedText type="defaultSemiBold">w</ThemedText> in the terminal running this project.
-			</ThemedText>
-		</Collapsible>
-
-		<Collapsible title="Images">
-			<ThemedText>
-			For static images, you can use the <ThemedText type="defaultSemiBold">@2x</ThemedText> and{' '}
-			<ThemedText type="defaultSemiBold">@3x</ThemedText> suffixes to provide files for
-			different screen densities
-			</ThemedText>
-			<Image source={require('@/assets/images/react-logo.png')} style={{ alignSelf: 'center' }} />
-			<ExternalLink href="https://reactnative.dev/docs/images">
-			<ThemedText type="link">Learn more</ThemedText>
-			</ExternalLink>
-		</Collapsible>
-
-		<Collapsible title="Custom fonts">
-			<ThemedText>
-			Open <ThemedText type="defaultSemiBold">app/_layout.tsx</ThemedText> to see how to load{' '}
-			<ThemedText style={{ fontFamily: 'SpaceMono' }}>
-				custom fonts such as this one.
-			</ThemedText>
-			</ThemedText>
-			<ExternalLink href="https://docs.expo.dev/versions/latest/sdk/font">
-			<ThemedText type="link">Learn more</ThemedText>
-			</ExternalLink>
-		</Collapsible>
-
-		<Collapsible title="Light and dark mode components">
-			<ThemedText>
-			This template has light and dark mode support. The{' '}
-			<ThemedText type="defaultSemiBold">useColorScheme()</ThemedText> hook lets you inspect
-			what the user's current color scheme is, and so you can adjust UI colors accordingly.
-			</ThemedText>
-			<ExternalLink href="https://docs.expo.dev/develop/user-interface/color-themes/">
-			<ThemedText type="link">Learn more</ThemedText>
-			</ExternalLink>
-		</Collapsible>
-
-		<Collapsible title="Animations">
-			<ThemedText>
-			This template includes an example of an animated component. The{' '}
-			<ThemedText type="defaultSemiBold">components/HelloWave.tsx</ThemedText> component uses
-			the powerful <ThemedText type="defaultSemiBold">react-native-reanimated</ThemedText>{' '}
-			library to create a waving hand animation.
-			</ThemedText>
-			{Platform.select({
-			ios: (
-				<ThemedText>
-				The <ThemedText type="defaultSemiBold">components/ParallaxScrollView.tsx</ThemedText>{' '}
-				component provides a parallax effect for the header image.
-				</ThemedText>
-			),
-			})}
-		</Collapsible>
-
-		</ParallaxScrollView>
+	</ThemedView>
 	);
 }
 
@@ -124,9 +93,9 @@ export default function TabTwoScreen() {
 // ####################################################################################
 
 const styles = StyleSheet.create({
-	body: {
-		backgroundColor: '#f0f0f0',
-	},
+	// body: {
+	// 	backgroundColor: '#f0f0f0',
+	// },
 
 	headerImage: {
 		color: '#808080',
@@ -137,13 +106,18 @@ const styles = StyleSheet.create({
 
 	titleContainer: {
 		flexDirection: 'row',
-		gap: 8,
+		gap: 0,
 	},
 
 	cajaLogo: {
 		flexDirection: 'column',
+		top: 0,
+		paddingTop: 25,
+		height: 150,
 		gap: 8,
 		alignItems: 'center',
+		alignContent: 'center',
+		justifyContent: 'center',
 	},
 
 	meLoPidoLogo: {
